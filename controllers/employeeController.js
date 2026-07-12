594e3377d210476e47984a4afcaa6a50a0adcb47
Master-mohit <mohitmansigupta9098@gmail.com>
2026-07-12 17:34:38 +0530

    add pagination

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -19,17 +19,25 @@ const createEmployee = async(req, res) => {
 }
 
 const getAllEmployees = async(req, res) => {
+
+
+     const page = Number(req.query.page)|| 1;
+     const limit = Number(req.query.limit)||1;
+
+       const skip = (page - 1) * limit;
+
         const employees = await employee.find({
-          salary: {
-               $gte: 8000
-          },
-          department: {
-               $in: ['IT', 'HR']
-          },
-          age :{
-               $gte: 23
-          }
-        }, "name email");
+
+          // salary: {
+          //      $gte: 8000
+          // },
+          // department: {
+          //      $in: ['IT', 'HR']
+          // },
+          // age :{
+          //      $gte: 23
+          // }
+        }, "name email").skip(skip).limit(limit);
             res.status(200).json({
                message: "Employees retrieved successfully",
                data: employees
